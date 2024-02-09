dc5a9100ab68ab2b50d75c2ae601a2d0d949bbc7
Bureau <Bureau@ZenBook>
2024-02-09 15:57:49 +0100

    Pagination et Tri fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCreateComponent } from './user-create/user-create.component';
 import { UserUpdateComponent } from './user-update/user-update.component';
-import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
+
 import { ArticleCreateComponent } from './article-create/article-create.component';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleUpdateComponent } from './article-update/article-update.component';
 
+
 @NgModule({
 	declarations: [
 		AppComponent,
-		UserListComponent,
 		UserCreateComponent,
+		UserListComponent,		
 		UserUpdateComponent,
 		ArticleCreateComponent,
 		ArticleListComponent,
@@ -26,7 +36,13 @@ import { ArticleUpdateComponent } from './article-update/article-update.componen
 		BrowserModule,
 		AppRoutingModule,
 		HttpClientModule,
-		FormsModule
+		FormsModule,
+		BrowserAnimationsModule,
+		MatTableModule,
+		MatPaginatorModule,
+		MatSortModule,
+		MatFormFieldModule,
+		MatInputModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
